b53a90912d1738f2c16d96e890d90a21ba18e6a6
gaoyehua <gaoyehua@crazymaplestudio.com>
2021-10-26 19:14:29 +0800

    commit

---
+++ Dev01/main.go
@@ -0,0 +1,21 @@
+package main // 声明 main 包，表明当前是一个可执行程序
+
+import "fmt" // 导入内置 fmt 包
+
+func main() { // main函数，是程序执行的入口
+	fmt.Println("Hello World!") // 在终端打印 Hello World!
+
+	var fly IFly = new(Bird)
+	fly.Fly()
+}
+
+type Bird struct {
+}
+
+func (b *Bird) Fly() {
+	fmt.Println("start to fly")
+}
+
+type IFly interface {
+	Fly()
+}
